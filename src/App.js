a23956361618ea26b080d24ba71397f0ba1a9466
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-12-02 16:07:07 +0700

    Router with component

--- src/App.js
+++ src/App.js
@@ -1,81 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
-import fire from './fire';
+import Register from './component/register';
+import Login from './component/login';
+import Navbar from './component/navbar';
+
 
 class App extends Component {
-  constructor(props){
-    super(props);
-      this.state = {
-      email : '',
-      password : '',
-      errorLogin : '',
-    }
-  }
   render() {
-    const login = (event) => {
-      event.preventDefault();
-      const email = this.state.email;
-      const password = this.state.password;
-      fire.auth()
-        .createUserWithEmailAndPassword(email, password)
-        .then((result) => {
-          console.log(result);
-          var user = fire.auth().currentUser;
-          if (user != null) {
-            user.providerData.forEach(function (profile) {
-              console.log("  Provider-specific UID: " + profile.uid);
-              console.log("  Email: " + profile.email);
-            });
-          }
-        })
-        .catch((error) => {
-          this.setState({errorLogin:error.message})
-        });
-    }
-    const inputHandle = (event) => {
-      const name = event.target.name;
-      const value = event.target.value;
-      this.setState({[name]: value, errorLogin: ''})
-    }
     return (
+      <Router>
       <div className="App">
-        <div className="navbar navbar-light bg-light">
-          <div className="navbar-brand">Firebase AUTH</div>
-          <div className="col-md-4">
-            <button className="btn btn-outline-success" >Login</button>
-          </div>
-        </div>
-        <div className="container">
-          <div className="col-md-5" style={{"margin": "auto"}}>
-            <form onSubmit={login}>
-              <h2 className="form-group">Login</h2>
-              <h5 className="form-group text-danger" id="errorLogin">{ this.state.errorLogin }</h5>
-              <div className="form-group">
-                <input type="email"
-                  className="form-control"
-                  id="email"
-                  name="email"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter email"
-                  onChange={(event) => { inputHandle(event) }}/>
-              </div>
-              <div className="form-group">
-                <input type="password"
-                  className="form-control"
-                  id="password"
-                  name="password"
-                  placeholder="Password"
-                  onChange={(event) => { this.setState({password: event.target.value}) }}/>
-              </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
-              <hr/>
-              <div className="form-group">
-                <span>Not have user? Register here.</span>
-              </div>
-            </form>
-          </div>
+        <Navbar />
+        <div>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Register} />
         </div>
       </div>
+      </Router>
     );
   }
 }
